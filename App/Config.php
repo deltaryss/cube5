13db9a7745834d7ffdd6b61f6dd60cc2dafe8fdb
deltaryss <solarylord@gmail.com>
2023-08-20 18:01:12 +0200

    docker

--- App/Config.php
+++ App/Config.php
@@ -14,7 +14,7 @@ class Config
      * Database host (localhost for local development or db for docker)
      * @var string
      */
-    const DB_HOST = 'db';
+    const DB_HOST = 'databse';
 
     /**
      * Database name
@@ -26,13 +26,13 @@ class Config
      * Database user
      * @var string
      */
-    const DB_USER = 'root';
+    const DB_USER = 'webapplication';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'root';
+    const DB_PASSWORD = '653rag9T';
 
     /**
      * Show or hide error messages on screen
